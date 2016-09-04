1c795209d4c90cbbae0b85cc7cdbde31c0bd8f36
丛林 <lincong.javatech@gmail.com>
2016-09-04 18:02:51 +0800

    Fixed build error on osx  (still can't use cta on mac... because that cta api so files not support osx.)

--- vn.ctp/vnctptd/vnctptd/vnctptd.h
+++ vn.ctp/vnctptd/vnctptd/vnctptd.h
@@ -15,6 +15,7 @@
 
 //Boost
 #define BOOST_PYTHON_STATIC_LIB
+#include <iostream>
 #include <boost/python/module.hpp>	//python封装
 #include <boost/python/def.hpp>		//python封装
 #include <boost/python/dict.hpp>	//python封装
@@ -193,15 +194,15 @@ class ConcurrentQueue
 {
 private:
 	queue<Data> the_queue;								//标准库队列
-	mutable mutex the_mutex;							//boost互斥锁
-	condition_variable the_condition_variable;			//boost条件变量
+	mutable boost::mutex the_mutexx;							//boost互斥锁
+	boost::condition_variable the_condition_variable;			//boost条件变量
 
 public:
 
 	//存入新的任务
 	void push(Data const& data)
 	{
-		mutex::scoped_lock lock(the_mutex);				//获取互斥锁
+		boost::mutex::scoped_lock lock(the_mutexx);				//获取互斥锁
 		the_queue.push(data);							//向队列中存入数据
 		lock.unlock();									//释放锁
 		the_condition_variable.notify_one();			//通知正在阻塞等待的线程
@@ -210,14 +211,14 @@ public:
 	//检查队列是否为空
 	bool empty() const
 	{
-		mutex::scoped_lock lock(the_mutex);
+		boost::mutex::scoped_lock lock(the_mutexx);
 		return the_queue.empty();
 	}
 
 	//取出
 	Data wait_and_pop()
 	{
-		mutex::scoped_lock lock(the_mutex);
+		boost::mutex::scoped_lock lock(the_mutexx);
 
 		while (the_queue.empty())						//当队列为空时
 		{
